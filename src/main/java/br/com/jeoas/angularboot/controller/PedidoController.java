d3faa59556415d0e86401462c6d5a40f170a1287
Jeoas Alves Moreira <jeoasalves@hotmail.com>
2019-11-15 21:07:12 -0300

    An @CrossOrigin annotation has been added to allow angular requests

--- src/main/java/br/com/jeoas/angularboot/controller/PedidoController.java
+++ src/main/java/br/com/jeoas/angularboot/controller/PedidoController.java
@@ -5,6 +5,7 @@ import static org.springframework.http.HttpStatus.OK;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RestController
 @RequestMapping(value = "/pedidos")
+@CrossOrigin
 public class PedidoController {
  
 	@Autowired
